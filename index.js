45ff09fcb15c7de79f2e318c1704142553e23ae5
Shudhanshu Shree <83982549+shudhanshu7324@users.noreply.github.com>
2024-06-12 16:39:07 +0530

    removed cors for using proxies in react frontend

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 import express from "express"
 import 'dotenv/config'
-import cors from 'cors';
+// import cors from 'cors';
 
 const app = express();
 
-const corsOptions ={
-    origin:'http://localhost:5173', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
+// const corsOptions ={
+//     origin:'http://localhost:5173', 
+//     credentials:true,            //access-control-allow-credentials:true
+//     optionSuccessStatus:200
+// }
 
-app.use(cors(corsOptions))
+// app.use(cors(corsOptions))
 
 app.get('/',(req,res)=>{
     res.send("Server is running");
